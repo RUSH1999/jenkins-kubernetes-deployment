42a01476c3bca450b35434c382380d45d63c0618
Rupesh Yadav <52694716+RUSH1999@users.noreply.github.com>
2026-05-21 10:37:11 +0530

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-
-echo "Redeploying to Kubernetes..."
-cd /mnt/e/Devops\ projects/Projects/testapp
-kubectl apply -f k8s-service.yaml
-kubectl apply -f k8s-deployment.yaml
-kubectl rollout status deployment/react-app --timeout=120s
-
